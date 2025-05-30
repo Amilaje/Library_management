086d2210f19944892148b86991fbeef1828fd8b8
songhyunjeong <songhyunjeong@gmail.com>
2025-05-30 13:39:49 +0900

    NotFound 페이지 추가 및 라우터에 연결

---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>404 ∘ ∘ ∘ ( °ヮ° ) ?</h1>
+      <p>페이지를 찾을 수 없습니다.</p>
+      <p>URL을 다시 확인해주세요.</p>
+    </div>
+  );
+}
