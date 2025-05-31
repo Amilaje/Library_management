64ad0cac9eb57a99ce1168c298d89cf085f673fd
songhyunjeong <songhyunjeong@gmail.com>
2025-05-31 17:36:38 +0900

    파일 정리

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,6 +1,7 @@
 // src/router.js
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import List from "./pages/List";
 import Publish from "./pages/Publish";
 import Edit from "./pages/Edit";
 import View from "./pages/View";
@@ -11,6 +12,7 @@ export default function Router() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/list" element={<List />} />
         <Route path="/publish" element={<Publish />} />
         <Route path="/edit/:id" element={<Edit />} />
         <Route path="/view/:id" element={<View />} />
